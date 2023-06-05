220163c2ed4bf4dd7ceb31616143155e89c04b9d
lothariong <lothariong@gmail.com>
2023-06-05 14:36:25 +0800

    Add project files.

---
+++ EmpowerMaoShanWang_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerMaoShanWang_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerMaoShanWang_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("C7D95565-C485-4081-9504-25C0BC32F32F")]
+[assembly: AssemblyVersion("1.0.0.0")]
